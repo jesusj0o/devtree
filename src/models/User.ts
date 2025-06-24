045fbe3fdf7242dabfc152026c04cc235593fa8b
Jesus Jo <jesusjho26@gmail.com>
2025-06-23 23:14:40 -0700

    User interface, schema and model created

---
+++ src/models/User.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+//un schema nos ayuda a agregar ciertas reglas que quieremos aplicar a los registros
+//se define el schema y despues se relaciona con su modelo 
+
+export interface IUser { 
+    name: string, 
+    email: string, 
+    password: string
+}
+
+
+const userSchema = new Schema({
+    name: { 
+        type: String, 
+        require: true,
+        trim: true
+    }, 
+    email: {
+        type: String, 
+        require: true,
+        trim: true,
+        unique: true
+    }, 
+    password: {
+        type: String, 
+        require: true,
+        trim: true
+    }
+});
+
+//definimos el modelo y usamos  Generics para usar nuestra Interface
+const User = mongoose.model<IUser>('User', userSchema); 
+export default User;
