21ee5a00ee8309c8250615a01e65c586c3f4c4b4
Jesus Jo <jesusjho26@gmail.com>
2025-06-24 20:15:50 -0700

    storing user in mongo instancing User from interface and model

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express';
+import User from './models/User';
 
 const router = Router(); 
 
 //Routing 
-router.post('/auth/register', (request, response) => {
-    
+router.post('/auth/register', async (request, response) => {
+    const user = new User(request.body);
+    await user.save();
 });
 
 
