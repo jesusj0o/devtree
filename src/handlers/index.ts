5d7761cbb96d4178cbaabdd2198dce14c862dca5
Jesus Jo <jesusjho26@gmail.com>
2025-06-26 20:36:05 -0700

    hash user's password to store it

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   const userExists = await User.findOne({ email });
   if (userExists) {
@@ -10,9 +11,10 @@ export const createAccount = async (req: Request, res: Response) => {
     res.status(409).json({ error: error.message });
     return;
   }
-  
+
   const user = new User(req.body);
- 
+  user.password = await hashPassword(password);
+
   await user.save();
   res.status(201).send("User created");
 };
---
+++ src/utils/auth.ts
@@ -0,0 +1,6 @@
+import bcript from 'bcrypt';
+
+export const hashPassword = async (password : string) => {
+    const salt = await bcript.genSalt(10);
+    return await bcript.hash(password, salt);
+}
